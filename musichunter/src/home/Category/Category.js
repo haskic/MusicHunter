5d82e86ee96e695817a80a85236c8755b484589c
Alexander Speek <kot3sany@gmail.com>
2020-07-04 17:21:22 +0300

    category parser added

--- musichunter/src/home/Category/Category.js
+++ musichunter/src/home/Category/Category.js
@@ -6,126 +6,139 @@ import song3 from './../../lose.mp3';
 import './../scss/Category.scss';
 import Slider from '../../slider/Slider';
 
-const testCategory = [{
-    title: "Charts: Top 50",
-    description: "The most played tracks on SoundCloud this week",
-    playlists: [
-        {
-            title: "All music genres",
-            description: "TOP 50",
-            playlist: [
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
-            ]
-        },
-        {
-            title: "All music genres",
-            description: "TOP 50",
-            playlist: [
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
-            ]
-        },
-        {
-            title: "All music genres",
-            description: "TOP 50",
-            playlist: [
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
-                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
-            ]
-        }
-    ]
-}];
+import testCategory from './testCategory';
 
-const arr = [];
-arr.push(<div className="slide-container">
-    <div className="category-carusel-item" >
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-</div>);
-arr.push(<div className="slide-container">
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-    <div className="category-carusel-item">
-        <div className="category-carusel-item-album-cover">
-            <img src={defaultCover}></img>
-        </div>
-        <div className="category-carusel-item-title">All music genres</div>
-        <div className="category-carusel-item-description">Top 50</div>
-    </div>
-</div>);
+// const arr = [];
+// arr.push(<div className="slide-container">
+//     <div className="category-carusel-item" >
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+// </div>);
+// arr.push(<div className="slide-container">
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+//     <div className="category-carusel-item">
+//         <div className="category-carusel-item-album-cover">
+//             <img src={defaultCover}></img>
+//         </div>
+//         <div className="category-carusel-item-title">All music genres</div>
+//         <div className="category-carusel-item-description">Top 50</div>
+//     </div>
+// </div>);
 
 function Category(props) {
-    const [categoryList, setcategoryList] = useState([])
+    const [categoryList, setcategoryList] = useState(categoryCaruselParser(testCategory, 4));
     function categoryUpdater(categoryArray) {
-        categoryArray.forEach(element => {
-            let el = <div></div>
-        });
+        // categoryArray.forEach(element => {
+        //     let el = <div></div>
+        // });
     }
-    function categoryCaruselParser() {
+    function splitIntoSubArray(arr, count) {
+        let copyarr = arr.slice(0);
+        var newArray = [];
+        while (copyarr.length > 0) {
+            newArray.push(copyarr.splice(0, count));
+        }
+        return newArray;
+    }
+    function categoryCaruselParser(categoryArray, elementsInOneSlide) {
+        let newCategoryList = [];
+        categoryArray.forEach(element => {
+            let newSlides = [];
+            let slidesSubArray = splitIntoSubArray(element.playlists, elementsInOneSlide);
+            console.log("SUB array  =", slidesSubArray);
+            slidesSubArray.forEach((slideElement, index) => {
+                newSlides.push(<div className="slide-container">
+                    {slideElement.map((value) => {
+                        return <div className="category-carusel-item">
+                            <div className="category-carusel-item-album-cover">
+                                <img src={defaultCover}></img>
+                            </div>
+                            <div className="category-carusel-item-title">{value.title}</div>
+                            <div className="category-carusel-item-description">{value.description}</div>
+                        </div>
+                    })}
+                    {/* {console.log("el = ", slideElement)} */}
+                </div>)
+            })
+            // element.playlists.forEach(el => {
+            //     newSlides.push(
+            //         <div className="category-carusel-item">
+            //             <div className="category-carusel-item-album-cover">
+            //                 <img src={defaultCover}></img>
+            //             </div>
+            //             <div className="category-carusel-item-title">{el.title}</div>
+            //             <div className="category-carusel-item-description">{el.description}</div>
+            //         </div>
+            //     );
+            // })
+            // for (let index = 0; index < element.playlists; index++) {
+            //     if (index % elementsInOneSlide === 0 && index !== 0) {
 
+            //     }
+
+            // }
+            console.log("NEW SLIDES", newSlides);
+            newCategoryList.push(<div className="category">
+                <div className="category-container">
+                    <div className="category-title">{element.title}</div>
+                    <div className="category-description">{element.description}</div>
+                    <div className="category-carusel">
+                        <Slider width="100%" height="300px" slides={newSlides} infinity={false} duration={410}></Slider>
+                    </div>
+                </div>
+            </div>);
+        });
+        // console.log("NEW SLIDES",newCategoryList);
+        // setcategoryList(newCategoryList);
+        return newCategoryList;
     }
     return (<div className="home-page-category">
-        <div className="category">
+        {/* <div className="category">
             <div className="category-container">
                 <div className="category-title">Charts: Top 50</div>
                 <div className="category-description">The most played tracks on SoundCloud this week</div>
@@ -142,7 +155,9 @@ function Category(props) {
                     <Slider width="100%" height="300px" slides={arr} infinity={false} duration={410}></Slider>
                 </div>
             </div>
-        </div>
+        </div> */}
+        {/* {categoryCaruselParser(testCategory,4)} */}
+        {categoryList}
     </div>)
 
 }
---
+++ musichunter/src/home/Category/testCategory.js
@@ -0,0 +1,226 @@
+import defaultCover from './../../AlbumCover.jpeg';
+import song2 from './../../shakira.mp3';
+import song3 from './../../lose.mp3';
+
+const testCategory = [{
+    title: "Charts: Top 50",
+    description: "The most played tracks on SoundCloud this week",
+    playlists: [
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 100",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        }
+    ]
+},{
+    title: "Charts: Top 50",
+    description: "The most played tracks on SoundCloud this week",
+    playlists: [
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 100",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 50",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 5",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        },
+        {
+            title: "All music genres",
+            description: "TOP 500",
+            playlist: [
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 },
+                { name: "Lose Yourself", artist: "Eminem", albumCoverImage: defaultCover, src: song3 }
+            ]
+        }
+    ]
+}];
+
+
+export default testCategory;
