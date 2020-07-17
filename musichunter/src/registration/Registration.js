2b1b1810bb4fc4dd20518fe8813a1281ed0d9d3c
Alexander Speek <kot3sany@gmail.com>
2020-07-17 12:46:18 +0300

    added model of registration form

--- musichunter/src/registration/Registration.js
+++ musichunter/src/registration/Registration.js
@@ -4,10 +4,19 @@ import React from 'react';
 
 
 
-function Registration(props){
-    
-    return(<div className="registration">
-        
+function Registration(props) {
+    return (<div className="registration">
+        <div className="reg-form">
+            <div className="reg-form-container">
+                <label for="email-input">
+                    <input type="text" name="email-input"></input>
+                </label>
+                <button>Continue</button>
+                <div className="helper">
+                    <span>Need help?</span>
+                </div>
+            </div>
+        </div>
     </div>)
 }
 
