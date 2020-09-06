a2fb1cc5d43f5086c08938bae1e46b65925acca9
Alexander Speek <kot3sany@gmail.com>
2020-09-06 21:05:05 +0300

    TrackViewer updated

---
+++ musichunter/src/profile/api/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+export default {
+
+    getTracks: function (userHash, token, callback) {
+        axios.get('https://localhost:5001/api/user/getTracks', {
+            params: {
+                userHash: userHash
+            },
+            headers: {
+                token: token
+            }
+        }).then((res) => {
+            callback(res);
+        });
+    }
+}
--- musichunter/src/profile/sections/trackViewer/TracksViewer.js
+++ musichunter/src/profile/sections/trackViewer/TracksViewer.js
@@ -1,6 +1,7 @@
-import React,{useEffect} from 'react'
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import Track from './Track';
-
+import API from './../../api/api';
 
 import './scss/TrackViewer.scss';
 import default_cover from './../../../AlbumCover.jpeg';
@@ -37,21 +38,41 @@ const default_track4 = {
 const default_track5 = {
     name: "Box Chevy 5",
     artist: "Yelawolf",
-    coverImage: default_cover,
+    coverImage: "https://localhost:5001/images/7390833756D88518B3DCFD42C64FAE9B72C21C69FFC89C1200CC75F664D7CF6D.jpg",
     src: default_music,
     hash: "qwerty5"
 }
 
-function TrackViewer(props){
-    return(<div className="track-viewer">
-        <Track track={default_track1}></Track>
+function TrackViewer(props) {
+
+    const [tracks, setTracks] = useState([]);
+    useEffect(() => {
+        API.getTracks(props.store.currentUser.hash, props.store.currentUser.token, (res) => {
+            setTracks(JSON.parse(res.data.tracks));
+            console.log("TRACK DATA = ", res);
+        });
+    }, [])
+    return (<div className="track-viewer">
+        {/* <Track track={default_track1}></Track>
         <Track track={default_track2}></Track>
         <Track track={default_track3}></Track>
         <Track track={default_track4}></Track>
-        <Track track={default_track5}></Track>
+        <Track track={default_track5}></Track> */}
+        {tracks.map && tracks.map((value) => {
+            console.log("IMAGE URL =",value.ImageUrl);
+            return <Track track={{
+                name: value.Name,
+                artist: value.Artist,
+                coverImage: "https://localhost:5001/images/" + value.ImageUrl,
+                src: "https://localhost:5001/music/" + value.HashUrl,
+                hash: value.HashUrl
+            }}></Track>
+        })}
 
     </div>);
 }
 
 
-export default TrackViewer;
+export default connect(
+    state => ({ store: state }),
+)(TrackViewer);
