db4aaba2a5b49e71f9dc92bc517cc5e257f86bfc
Alexander Speek <kot3sany@gmail.com>
2020-10-18 16:26:41 +0300

    search-viewer style updated

--- musichunter/src/search/SearchViewer/SearchViewer.js
+++ musichunter/src/search/SearchViewer/SearchViewer.js
@@ -21,21 +21,19 @@ const menuItems = [
 ]
 
 
-function SearchViewer() {
+function SearchViewer(props) {
 
     const [selectedMenuItem, setSelectedMenuItem] = useState(0);
 
     return (
         <div className="search-page">
             <div className="search-page-container">
-                <div className="top-search-title">Search results for "asdfasdf"</div>
+                <div className="top-search-title">Search results for "{props.searchText}"</div>
                 <div className="search-page-container-subcontainer">
                     <div className="search-left-menu">
                         <div className="menu">
                             <ul>
-
                                 {menuItems.map((item, index) => {
-
                                     return <li onClick={() => setSelectedMenuItem(index)}>
                                         <div className={index == selectedMenuItem ? "menu-item-selected" : "menu-item"}>{item.text}</div>
                                         {index == selectedMenuItem ? <div class="arrow-right"></div> : null}
@@ -43,6 +41,10 @@ function SearchViewer() {
                                     </li>
                                 })}
                             </ul>
+                        </div>
+                        <div className="search-left-menu-description">
+                                <div className="language-button">Language:</div>
+                                <div className="language-selected">English (US)</div>
                         </div>
                     </div>
                     <div className="search-content">
